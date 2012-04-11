968f637b35e0100c4d2a63ccdba88a140aab25dc
Hugo <hugoshi@blob.(none)>
2012-04-11 14:29:06 -0400

    added client for proxy

--- src/test/webzmqproxy_test.py
+++ src/test/webzmqproxy_test.py
@@ -2,6 +2,7 @@ import gevent
 import gevent.monkey
 gevent.monkey.patch_all()
 from gevent_zeromq import zmq
+
 import unittest
 import test_utils as test_utils
 
@@ -36,5 +37,20 @@ class ProxyTestCase(unittest.TestCase):
 		result = test_utils.recv_timeout(sub, 10000)
 		print result
 		assert result == ['hello']
-
 		
+	def test_proxy_client(self):
+		ctx = zmq.Context()
+		repsocket = ctx.socket(zmq.REP)
+		repsocket.bind(reqrep)
+		proxy = webzmqproxy.Proxy(reqrep, pushpull, pubsub, ctx=ctx)
+		proxy.start()
+		t = threading.Thread(target=self.echo, args = (repsocket,))
+		t.start()
+		client = webzmqproxy.ProxyClient(pushpull, pubsub, ctx=ctx)
+		client.start()
+		result = client.request(['hello'])
+		assert result == ['hello']
+		
+
+if __name__ == "__main__":
+	unittest.main()
--- src/webzmqproxy.py
+++ src/webzmqproxy.py
@@ -2,8 +2,72 @@ import gevent
 import gevent.monkey
 gevent.monkey.patch_all()
 from gevent_zeromq import zmq
-import threading
+from gevent.queue import Queue
 
+import threading
+import uuid
+import time
+
+
+class ProxyClient(threading.Thread):
+	def __init__(self, pushpulladdr, pubsubaddr, timeout=0.5, ctx=None):
+		if ctx is None:
+			ctx = zmq.Context()
+		self.ctx = ctx
+		self.pushpulladdr = pushpulladdr
+		self.pubsubaddr = pubsubaddr
+		self.queues = {}
+		self.send_queue = Queue()
+		self.timeout = timeout
+		super(ProxyClient, self).__init__()
+		
+	def run_send(self):
+		print 'runsend'
+		self.push = self.ctx.socket(zmq.PUSH)
+		self.push.connect(self.pushpulladdr)
+		counter = 0.
+		while True:
+			print 'runing send'
+			try:
+				messages = self.send_queue.get(timeout=self.timeout)
+			except:
+				counter += 1
+				if counter < 20:
+					continue
+				else:
+					break
+			print 'sending', messages
+			self.push.send_multipart(messages)
+	
+	def run_recv(self):
+		self.sub = self.ctx.socket(zmq.SUB)
+		self.sub.setsockopt(zmq.SUBSCRIBE,'')
+		self.sub.connect(self.pubsubaddr)
+		print 'subloop'
+		while True:
+			messages = self.sub.recv_multipart()
+			print 'sub received', messages
+			ident = messages[-1]
+			messages = messages[:-1]
+			if ident in self.queues:
+				self.queues[ident].put(messages)
+			
+	def run(self):
+		t = threading.Thread(target=self.run_send)
+		t.start()
+		self.run_recv()
+		t.join()
+		
+	def request(self, messages):
+		ident = str(uuid.uuid4())
+		queue = Queue()
+		self.queues[ident] = queue
+		messages.append(ident)
+		self.send_queue.put(messages)
+		returnval = self.queues[ident].get()
+		del self.queues[ident]
+		return returnval
+	
 class Proxy(threading.Thread):
 	def __init__(self, reqrepaddr, pushpulladdr, pubsubaddr, ctx=None):
 		if ctx is None:
