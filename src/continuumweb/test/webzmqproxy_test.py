7abbfe03c25d64c7b7649c18f692b9cbf988abfe
Hugo <hugoshi@blob.(none)>
2012-04-12 09:32:51 -0400

    tabs snuck in here

--- src/continuumweb/test/webzmqproxy_test.py
+++ src/continuumweb/test/webzmqproxy_test.py
@@ -13,44 +13,44 @@ reqrep = "inproc://#2"
 pushpull = "inproc://#3"
 
 class ProxyTestCase(unittest.TestCase):
-	def echo(self, socket):
-		while True:
-			m = socket.recv_multipart()
-			print 'echo received', m
-			socket.send_multipart(m)
-			
-	def test_proxy(self):
-		ctx = zmq.Context()
-		repsocket = ctx.socket(zmq.REP)
-		repsocket.bind(reqrep)
-		proxy = webzmqproxy.Proxy(reqrep, pushpull, pubsub, ctx=ctx)
-		proxy.start()
-		t = threading.Thread(target=self.echo, args = (repsocket,))
-		t.start()
+    def echo(self, socket):
+        while True:
+            m = socket.recv_multipart()
+            print 'echo received', m
+            socket.send_multipart(m)
+            
+    def test_proxy(self):
+        ctx = zmq.Context()
+        repsocket = ctx.socket(zmq.REP)
+        repsocket.bind(reqrep)
+        proxy = webzmqproxy.Proxy(reqrep, pushpull, pubsub, ctx=ctx)
+        proxy.start()
+        t = threading.Thread(target=self.echo, args = (repsocket,))
+        t.start()
 
-		push = ctx.socket(zmq.PUSH)
-		sub = ctx.socket(zmq.SUB)
-		sub.setsockopt(zmq.SUBSCRIBE,'')
-		push.connect(pushpull)
-		sub.connect(pubsub)
-		push.send('hello')
-		result = test_utils.recv_timeout(sub, 10000)
-		print result
-		assert result == ['hello']
-		
-	def test_proxy_client(self):
-		ctx = zmq.Context()
-		repsocket = ctx.socket(zmq.REP)
-		repsocket.bind(reqrep)
-		proxy = webzmqproxy.Proxy(reqrep, pushpull, pubsub, ctx=ctx)
-		proxy.start()
-		t = threading.Thread(target=self.echo, args = (repsocket,))
-		t.start()
-		client = webzmqproxy.ProxyClient(pushpull, pubsub, ctx=ctx)
-		client.start()
-		result = client.request(['hello'])
-		assert result == ['hello']
-		
+        push = ctx.socket(zmq.PUSH)
+        sub = ctx.socket(zmq.SUB)
+        sub.setsockopt(zmq.SUBSCRIBE,'')
+        push.connect(pushpull)
+        sub.connect(pubsub)
+        push.send('hello')
+        result = test_utils.recv_timeout(sub, 10000)
+        print result
+        assert result == ['hello']
+        
+    def test_proxy_client(self):
+        ctx = zmq.Context()
+        repsocket = ctx.socket(zmq.REP)
+        repsocket.bind(reqrep)
+        proxy = webzmqproxy.Proxy(reqrep, pushpull, pubsub, ctx=ctx)
+        proxy.start()
+        t = threading.Thread(target=self.echo, args = (repsocket,))
+        t.start()
+        client = webzmqproxy.ProxyClient(pushpull, pubsub, ctx=ctx)
+        client.start()
+        result = client.request(['hello'])
+        assert result == ['hello']
+        
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
