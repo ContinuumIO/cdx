177bb07ef8cd44751ec8687b094a536c06a26b3d
Hugo <hugoshi@blob.(none)>
2012-05-01 17:16:02 -0500

    defered view rendering in datatable example

--- src/blazeweb/static/js/test.js
+++ src/blazeweb/static/js/test.js
@@ -2,5 +2,5 @@ $(function(){
     table = Continuum.Collections['Table'].create(table_obj);
     view = new table.default_view({'model' : table});
     $('body').append(view.el);
-    view.render();
+    _.defer(function(){view.render()})
 });
