ba48780526fc70781af3b25dd655ce8020dd1333
Hugo <hugoshi@blob.(none)>
2012-06-22 21:11:41 -0400

    removing tabs

--- cloudblaze/blazeweb/views/main.py
+++ cloudblaze/blazeweb/views/main.py
@@ -1,6 +1,6 @@
 from flask import (
-	render_template, request, current_app,
-	send_from_directory, make_response)
+    render_template, request, current_app,
+    send_from_directory, make_response)
 import flask
 import os
 import simplejson
@@ -18,14 +18,14 @@ import cloudblaze.blazeweb.wsmanager as wsmanager
 #main pages
 @app.route('/')
 def index():
-	return render_template('index.html') 
+    return render_template('index.html') 
 
 @app.route('/favicon.ico')
 def favicon():
-	return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/x-icon')
-	
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/x-icon')
+    
 @app.route('/pageRender/<filename>')
 def pageRender(filename):
-	app.logger.debug('pageRender filename=[%s]',filename)
+    app.logger.debug('pageRender filename=[%s]',filename)
     # Note the corresponding html file must be in the templates folder.
-	return render_template(filename + '.html') 
+    return render_template(filename + '.html') 
