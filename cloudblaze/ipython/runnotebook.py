f073e889b89de328189e86f5e57741bfaaa3c3e1
Hugo <hugoshi@blob.(none)>
2012-06-24 19:30:29 -0400

    prevent notebook from popping open browser on startup

--- cloudblaze/ipython/runnotebook.py
+++ cloudblaze/ipython/runnotebook.py
@@ -2,6 +2,7 @@ import IPython.frontend.html.notebook.notebookapp as notebookapp
 from tornado import httpserver
 import tornado
 app = notebookapp.NotebookApp.instance()
+app.open_browser = False
 def launch_new_instance():
     app.initialize()
     app.start()
