14ce67deffb3e3b99dc87548323d4ee381fb0475
Paddy Mullen <paddy@paddymullen.com>
2012-07-03 17:28:28 -0400

    removed tabs

--- cloudblaze/blazeweb/views/backbone.py
+++ cloudblaze/blazeweb/views/backbone.py
@@ -1,7 +1,7 @@
 from cloudblaze.blazeweb.app import app
 from flask import (
-	render_template, request, current_app,
-	send_from_directory, make_response)
+        render_template, request, current_app,
+        send_from_directory, make_response)
 import flask
 import os
 import simplejson
@@ -21,14 +21,14 @@ def bulk_upsert(docid):
     models = [bbmodel.ContinuumModel(x['type'], **x['attributes']) \
               for x in data]
     for m in models:
-	if m.get('docs') is None:
-		m.set('docs', [docid])
-	m.set('created', True)	    
+        if m.get('docs') is None:
+            m.set('docs', [docid])
+        m.set('created', True)
         current_app.collections.add(m)
     docs = set()
     for m in models:
         docs.update(m.get('docs'))
-    clientid = request.headers.get('Continuum-Clientid', None)    
+    clientid = request.headers.get('Continuum-Clientid', None)
     for doc in docs:
         relevant_models = [x for x in models if doc in x.get('docs')]
         current_app.wsmanager.send(doc, app.ph.serialize_web(
@@ -46,7 +46,7 @@ def create(docid, typename):
     modeldata = current_app.ph.deserialize_web(request.data)
     model = bbmodel.ContinuumModel(typename, **modeldata)
     if model.get('docs') is None:
-	model.set('docs', [docid])
+        model.set('docs', [docid])
     model.set('created', True)
     current_app.collections.add(model)
     clientid=request.headers.get('Continuum-Clientid', None)
@@ -63,7 +63,7 @@ def put(docid, typename, id):
     modeldata = current_app.ph.deserialize_web(request.data)
     model = bbmodel.ContinuumModel(typename, **modeldata)
     if model.get('docs') is None:
-	model.set('docs', [docid])
+        model.set('docs', [docid])
     current_app.collections.add(model)
     clientid=request.headers.get('Continuum-Clientid', None)
     for doc in model.get('docs'):
@@ -88,7 +88,7 @@ def get(docid, typename=None, id=None):
             return app.ph.serialize_web([x.to_json() for x in models])
         else:
             return app.ph.serialize_web([x.to_broadcast_json() for x in models])
-        
+
 @app.route("/bb/<docid>/<typename>/<id>", methods=['DELETE'])
 def delete(docid, typename, id):
     model = current_app.collections.get(typename, id)
