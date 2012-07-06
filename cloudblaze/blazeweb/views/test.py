81105341ecd875c396f9bd82b197336d03ad48d4
Hugo <hugoshi@blob.(none)>
2012-07-06 10:30:25 -0400

    scrap file was breaking nosetests

--- cloudblaze/blazeweb/views/test.py
+++
@@ -1,9 +0,0 @@
-from cloudblaze.blazeweb.app import app
-from flask import (
-	render_template, request, current_app,
-	send_from_directory, make_response, session)
-
-@app.route('/authtest', methods=['GET'])
-def test():
-    print 'authtest', session
-    return session.get('username', 'WRONG')
