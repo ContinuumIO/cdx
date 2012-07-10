f6a746c992fb52729148d9d64bb5ded7c241bffc
Hugo <hugoshi@blob.(none)>
2012-07-10 16:45:55 -0400

    more cleanup

--- cloudblaze/blazeweb/static/js/test.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    table = Continuum.Collections['Table'].create(table_obj);
-    view = new table.default_view({'model' : table});
-    $('body').append(view.el);
-    _.defer(function(){view.render()})
-});
