ContinuumIO/cdx

commit 7af64534ff56dbf85801f09f2a4a1ffde6cd7f4a

more python prompt to right panel

cloudblaze/blazeweb/static/coffee/cdx.coffee
@@ -10,13 +10,13 @@ window.$CDX.resizeRoot = () ->
   winHeight = $(window).height()
   winWidth = $(window).width()
   cdxRootHeight=(winHeight * .85)
-  midPanelHeight = (cdxRootHeight * .65)
-  pyEdPaneHeight = (cdxRootHeight * .20)
+  midPanelHeight = (cdxRootHeight * .80)
+  pyEdPaneHeight = midPanelHeight
 
   $('#cdxRoot').height(cdxRootHeight)
   $('.midPanel').height(midPanelHeight)
   $('#cdxMidContainer').width(winWidth * .95)
-  $('.cdx-py-pane').width(winWidth * .85)
+  #$('.cdx-py-pane').width(winWidth * .85)
   $('.cdx-py-pane').height(pyEdPaneHeight)
 
 $CDX.resize_loop = () ->
@@ -225,43 +225,15 @@ $(() ->
   class SummaryView extends Backbone.View
     render: () ->
       console.log('summaryView render')
-      sample_data1 = [{
-        url: "/blaze/data/gold.hdf5/20100114/dates", type:"BlazeArrayProxy",
-        name:"dates"}
-        ,
-        {colsummary: {
-          0:{
-            std:6759.325780745387, max:1263502799,
-            mean:1263491099.9993594, min:1263479400}}
-          ,
-          summary:{
-            shape:[1561], colnames:[0]}}]
-          
-      sample_data2 = [{url: "/blaze/data/gold.hdf5/20100115/dates",
-      type:"BlazeArrayProxy", name:"dates"},
-      {colsummary: {0:{std:6759.325780745387, max:1263502799,
-      mean:1263491099.9993594, min:1263479400}},
-      summary:{shape:[2561], colnames:[0]}}]
-      #console.log(sample_data)
-      sa = [sample_data1, sample_data2]
       summary_template = $('#variable-summary-template').html()
-      snip = ''
-      for sa_ele in sa
-        snip += _.template2(summary_template, {item:sa_ele})
-
       snip2 = ''
       $.when($CDX.blaze.get_summary($CDX.IPython.namespace.get('variables'), (array) ->
-          for sa_ele in array
-            snip2 += _.template2(summary_template, {item:sa_ele})
-          console.log('snip2 namespace callback snip2', snip2)))
+        for sa_ele in array
+          snip2 += _.template2(summary_template, {item:sa_ele})
+        ))
       .then(=>
-          $(this.el).html("<h3> snip2 </h3>" + snip2))
+        $(this.el).html(snip2) )
       return $(this.el)
-
-
-  class BazView extends Backbone.View
-    render: () ->
-      return "<h3> baz view </h3>"
 
 
   $CDX.summaryView = new SummaryView()

cloudblaze/blazeweb/templates/cdx.html
@@ -39,40 +39,6 @@
 
 
 </script>
-<script type="text/template" id="publish-selection">
-  <div class="modal">
-    <div class="modal-header">
-      <h3>Publish Tabs</h3>
-    </div>
-    <div class="modal-body">
-      <form class="well">
-        <label>Choose Some tabs to export</label>
-	<% if (plots.length == 0){ %>
-	  <label> no plots to export </label>
-	<% }else{ %>
-	  <label> Plots </label>
-  	  <% _.each(plots, function(plot){ %>
-            <label class="checkbox">
-              <input class="tabselect" type="checkbox" tab="<%= plot %>"/> <%= plot %>
-            </label>
-	  <% }); %>
-	<% } %>
-	<% if (arrays.length == 0){ %>
-	  <label> no arrays to export </label>
-	<% }else{ %>
-	  <label> Arrays </label>
-  	  <% _.each(arrays, function(plot){ %>
-            <label class="checkbox">
-              <input class="tabselect" type="checkbox" tab="<%= plot %>"/> <%= plot %>
-            </label>
-	  <% }); %>
-	<% } %>
-	
-	<input class="publishsubmit" type="button" value="Submit"/>
-      </form>
-    </div>
-  </div>
-</script>
 <script type="text/template" id="variable-summary-template">
   <div class="well">
   <h3> <%= item[0].name %> </h3>
@@ -136,7 +102,7 @@
         </li>
         <li class=""><a href="#plots">Plot</a></li>
         <li class=""><a href="#analyze">Analyze</a></li>
-        <li class=""><a class="js-navigate" data-route_target="/cdx/unknown/sharecurrent">Share</a></li>
+        <li class=""><a href="#share">Share</a></li>
         <li class=""><a href="#help">Help</a></li>
       </ul>
     </div>
@@ -144,74 +110,42 @@
     <div id="cdxMidContainer" class="container span12">
       <div id="cdxMid" class="row-fluid">
         <div class="tabbable span2 cdx-panel-root">
-	  <ul class="nav nav-tabs">
-	    <li class="active"><a href="#dep-tabs1-pane1" data-toggle="tab">Cols</a></li>
-	    <li><a href="#dep-tabs1-pane2" data-toggle="tab">Func</a></li>
-	    <li><a href="#dep-tabs1-pane3" data-toggle="tab">Vars</a></li>
-	  </ul>
-	  <div class="tab-content">
-	    <div class="tab-pane active midPanel" id="dep-tabs1-pane1">
-	      <table class="table-bordered">
-	        <thead><th>name</th><th>range</th></thead>
-	        <tr><td>blah</td><td>0-100</td></tr>
-	        <tr><td>blah</td><td>0-100</td></tr>
-	        <tr><td>blah</td><td>0-100</td></tr>
-	        <tr><td>blah</td><td>0-100</td></tr>
-	        <tr><td>blah</td><td>0-100</td></tr>
-	      </table>
-	    </div>
-	    <div class="tab-pane midPanel" id="dep-tabs1-pane2">
-	      <h4>Pane 2 Content</h4>
-	      <p>Duis congue viverra arcu, ac aliquet turpis rutrum a. Donec semper vestibulum dapibus. Integer et sollicitudin metus. Vivamus at nisi turpis.</p>
-	    </div>
-	    <div class="tab-pane midPanel" id="dep-tabs1-pane3">
-	      <h4>Pane 3 Content</h4>
-	      <p>Phasellus eget justo lacus. Vivamus pharetra ullamcorper massa, nec ultricies metus gravida egestas. Duis congue viverra arcu, ac aliquet turpis rutrum a. Donec semper vestibulum dapibus. Integer et sollicitudin metus. Vivamus at nisi turpis.</p>
-	    </div>
-	  </div><!-- /.tab-content -->
-        </div>
-
-
-        <div id="main-tab-area" class="tabbable span8 cdx-panel-root">
-          
-        </div>
-
-
-
-        <div class="tabbable span2 cdx-panel-root">
-	  <ul class="nav nav-tabs">
-	    <li class="active"><a href="#dsp-tabs1-pane1" data-toggle="tab">View</a></li>
-	    <li><a href="#dsp-tabs1-pane2" data-toggle="tab">Quick</a></li>
-	  </ul>
-	  <div class="tab-content">
-	    <div class="tab-pane act  sample_data = [{url: "/blaze/data/gold.hdf5/20100114/dates",
-  type:"BlazeArrayProxy", name:"dates"}, 
-  {colsummary: {0:{std:6759.325780745387, max:1263502799, 
-  mean:1263491099.9993594, min:1263479400}}, 
-  summary:{shape:[1561], colnames:[0]}}];
-
-  console.log(sample_data)
-  summary_template = $('#variable-summary-template').html()
-
-  console.log(_.template(summary_template, {item:sample_data}))
-ive midPanel" id="dsp-tabs1-pane1">
-	      <table class="table-bordered">
-	        <thead><th>name</th><th>range</th></thead>
-	        <tr><td>blah</td><td>0-100</td></tr>
-	        <tr><td>blah</td><td>0-100</td><tr>
-	      </table>
-	    </div>
-	    <div class="tab-pane midPanel">
-	      <h4>Pane 2 Content</h4>
-	      <p>Phasellus eget justo lacus. Vivamus pharetra ullamcorper massa, nec ultricies metus gravida egestas. Duis congue viverra arcu, ac aliquet turpis rutrum a. Donec semper vestibulum dapibus. Integer et sollicitudin metus. Vivamus at nisi turpis.</p>
-	    </div>
-	  </div><!-- /.tab-content -->
-        </div>
+	    <ul class="nav nav-tabs">
+	      <li class="active"><a href="#dep-tabs1-pane1" data-toggle="tab">Cols</a></li>
+	      <li><a href="#dep-tabs1-pane2" data-toggle="tab">Func</a></li>
+	      <li><a href="#dep-tabs1-pane3" data-toggle="tab">Vars</a></li>
+	    </ul>
+	    <div class="tab-content">
+	      <div class="tab-pane active midPanel" id="dep-tabs1-pane1">
+	        <table class="table-bordered">
+	          <thead><th>name</th><th>range</th></thead>
+	          <tr><td>blah</td><td>0-100</td></tr>
+	          <tr><td>blah</td><td>0-100</td></tr>
+	          <tr><td>blah</td><td>0-100</td></tr>
+	          <tr><td>blah</td><td>0-100</td></tr>
+	          <tr><td>blah</td><td>0-100</td></tr>
+	        </table>
+	      </div>
+	      <div class="tab-pane midPanel" id="dep-tabs1-pane2">
+	        <h4>Pane 2 Content</h4>
+	        <p>Duis congue viverra arcu, ac aliquet turpis rutrum a. Donec semper vestibulum dapibus. Integer et sollicitudin metus. Vivamus at nisi turpis.</p>
+	      </div>
+	      <div class="tab-pane midPanel" id="dep-tabs1-pane3">
+	        <h4>Pane 3 Content</h4>
+	        <p>Phasellus eget justo lacus. Vivamus pharetra ullamcorper massa, nec ultricies metus gravida egestas. Duis congue viverra arcu, ac aliquet turpis rutrum a. Donec semper vestibulum dapibus. Integer et sollicitudin metus. Vivamus at nisi turpis.</p>
+	      </div>
+	    </div><!-- /.tab-content -->
       </div>
-    </div>
 
-    <div id="cdxPyPane" class="span12 cdx-py-pane" style="overflow-y:scroll;overflow-x:hidden">
-      {% include 'ipython.html' %}
+      <div id="main-tab-area" class="tabbable span5 cdx-panel-root"></div>
+
+      <div class="span5 well">
+      <div id="cdxPyPane" class="cdx-py-pane">
+        {% include 'ipython.html' %}
+      </div>
+      </div>
+
+      </div>
     </div>
 
     <div id="cdxBottomMsg" class="row-fluid"></div>

cloudblaze/blazeweb/static/css/cdx.css
@@ -18,8 +18,13 @@ overflow: auto;
 .cdx-dep {
 }
 
-.cdx-py-editor {
+.cdx-py-pane {
+    overflow-y: scroll;
+    overflow-x: hidden;
     margin: 10px;
+}
+
+.tmp {
     min-height: 50px;
     background-color: black;
     text-color: white;
@@ -158,7 +163,7 @@ overflow: auto;
 .pane-holder > .pane {
 
   /*border:1px solid red; */
-  height:400px;
+  height:500px;
   overflow: auto;
 }
 

cloudblaze/blazeweb/templates/base.html
@@ -86,10 +86,10 @@
 	<div id="mainContainer">
 	  {% block main %}
 	  {% endblock %}
-	</div>
-	<footer class="row-fluid">
-		<p>&copy; Continuum Analytics 2012</p>
+  </div>
+  <hr>
+	<footer class="span12">
+	 <p>&copy; Continuum Analytics 2012</p>
 	</footer>
-
   </body>
 </html>

commit 49d6b0b7449a27ace23b751bff3ce296924e4d1e

publish functionality got broken in 7af64534ff56dbf85801f09f2a4a1ffde6cd7f4a

cloudblaze/blazeweb/static/coffee/cdx.coffee
@@ -146,6 +146,7 @@ $(() ->
       "cdx/:docid/viz/:plot_id": "load_specific_viz"
       "cdx/:docid/published/:modelid" : "load_published"
       },
+
     load_published : (docid, modelid) ->
       if not $CDX._doc_loaded.isResolved()
         $CDX.utility.start_instatiate(docid)
@@ -404,7 +405,7 @@ class PublishView extends Continuum.ContinuumView
     @arrays = {}
     @render()
   render : () ->
-    Continuum.build_views(@model, @plots, @mget('plots'))
+    Continuum.build_views(@model, @plots, @mget('plots'), {'render_loop':true})
     Continuum.build_views(@model, @arrays, @mget('arrays'))
     for info, idx in @mget('plot_tab_info')
       plotid = @mget('plots')[idx].id

cloudblaze/blazeweb/templates/cdx.html
@@ -30,6 +30,41 @@
 <script type=text/javascript src="{{ url_for('static',filename='js/ipython.js') }}"></script>
 <script type=text/javascript src="{{ url_for('static',filename='js/blaze.js') }}"></script>
 
+<script type="text/template" id="publish-selection">
+  <div class="modal">
+    <div class="modal-header">
+      <h3>Publish Tabs</h3>
+    </div>
+    <div class="modal-body">
+      <form class="well">
+        <label>Choose Some tabs to export</label>
+	<% if (plots.length == 0){ %>
+	  <label> no plots to export </label>
+	<% }else{ %>
+	  <label> Plots </label>
+  	  <% _.each(plots, function(plot){ %>
+            <label class="checkbox">
+              <input class="tabselect" type="checkbox" tab="<%= plot %>"/> <%= plot %>
+            </label>
+	  <% }); %>
+	<% } %>
+	<% if (arrays.length == 0){ %>
+	  <label> no arrays to export </label>
+	<% }else{ %>
+	  <label> Arrays </label>
+  	  <% _.each(arrays, function(plot){ %>
+            <label class="checkbox">
+              <input class="tabselect" type="checkbox" tab="<%= plot %>"/> <%= plot %>
+            </label>
+	  <% }); %>
+	<% } %>
+	
+	<input class="publishsubmit" type="button" value="Submit"/>
+      </form>
+    </div>
+  </div>
+</script>
+
 <script type="text/template" id="tree-template">
   <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal">×</button>
@@ -106,7 +141,7 @@
         </li>
         <li class=""><a href="#plots">Plot</a></li>
         <li class=""><a href="#analyze">Analyze</a></li>
-        <li class=""><a href="#share">Share</a></li>
+        <li class=""><a class="js-navigate" data-route_target="/cdx/unknown/sharecurrent">Share</a></li>	
         <li class=""><a href="#help">Help</a></li>
       </ul>
     </div>