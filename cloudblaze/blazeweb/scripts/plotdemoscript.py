ca1f7351791c96d58bc6d1f2f1889f0b9290f24e
Hugo <hugoshi@blob.(none)>
2012-07-17 10:45:24 -0400

    script for scipy 2012 lightning talk demo

---
+++ cloudblaze/blazeweb/scripts/plotdemoscript.py
@@ -0,0 +1,35 @@
+prices = da_hdf5_20100111
+prices = prices.seval()
+
+GLD_ylds = prices['GLD'][4::4] / prices['GLD'][:-4:4]
+GLD = prices['GLD'][4::4]
+GLD = GLD / GLD[0]
+GDX_ylds = prices['GDX'][4::4] / prices['GDX'][:-4:4]
+GDX = prices['GDX'][4::4]
+GDX = GDX/GDX[0]
+USO_ylds = prices['USO'][4::4] / prices['USO'][:-4:4]
+USO = prices['USO'][4::4]
+USO = USO/USO[0]
+timestamp = prices['timestamp'][4::4]
+
+source = p.make_source(GLD_ylds=GLD_ylds, GLD=GLD, GDX_ylds=GDX_ylds, GDX=GDX, USO=USO, USO_ylds=USO_ylds, timestamp=timestamp)
+
+gldgdx = p.scatter(x='GLD_ylds', y='GDX_ylds', title='GLD vs GDX', data_source=source)
+glduso = p.scatter(x='GLD_ylds', y='USO_ylds', title='GLD vs USO', data_source=source)
+gdxuso = p.scatter(x='GDX_ylds', y='USO_ylds', title='GDX vs USO', data_source=source)
+
+lineplot = p.scatter(x='timestamp', y='GDX', color="#F00", title="timeseries", data_source=source, width=800, height=300,)
+p.scatter(x='timestamp', y='GLD', color="#0F0", data_source=source, scatterplot=lineplot)
+p.scatter(x='timestamp', y='USO', color="#00F", data_source=source, scatterplot=lineplot)
+p.line(x='timestamp', y='GDX', data_source=source, lineplot=lineplot)
+p.line(x='timestamp', y='GLD', data_source=source, lineplot=lineplot)
+p.line(x='timestamp', y='USO', data_source=source, lineplot=lineplot)
+
+p.updateic()
+densities = p.grid([[gldgdx, glduso, gdxuso]], title='densities')
+
+
+
+
+
+
