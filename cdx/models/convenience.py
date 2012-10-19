ce46c61038f0e23abeb61c542592ccfb20515364
Hugo <hugoshi@blob.(none)>
2012-10-18 20:01:30 -0400

    forgot this file

---
+++ cdx/models/convenience.py
@@ -0,0 +1,38 @@
+import uuid
+
+from . import user
+from . import docs
+
+def from_wakari(app, request, session=None):
+    """reads django user information, if a user is present,
+    we create/ensure an equivalent one lives in our DB.
+    We'll call this every time for now.. but we can be more
+    efficient about it in the future
+    """
+    from wakariserver.djangointerface import (
+        get_session_data,
+        get_user,
+        get_current_user,
+        login_required
+        )
+    try:
+        dbsession = session if session else app.Session() 
+        auth_user, wakari_user = get_current_user(dbsession, request)
+        if auth_user is None or wakari_user is None:
+            return None
+        else:
+            cdxuser = user.User.load(app.model_redis, auth_user.username)
+            if cdxuser is None:
+                docid = str(uuid.uuid4())
+                doc = docs.new_doc(app, docid, 'main', [auth_user.username])
+                cdxuser = user.new_user(
+                    app.model_redis,
+                    auth_user.username,
+                    str(uuid.uuid4()),
+                    docs=[doc.docid])
+            return cdxuser
+    finally:
+        #close session if it was not passed  in
+        if not session:
+            dbsession.close()
+
