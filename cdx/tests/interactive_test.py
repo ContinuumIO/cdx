230079e13975febc9a70f95528f216701fb63a19
Hugo <hugoshi@blob.(none)>
2012-10-21 13:47:16 -0400

    cdx bump

--- cdx/tests/interactive_test.py
+++ cdx/tests/interactive_test.py
@@ -2,12 +2,17 @@ from cdx import plot
 p = plot.PlotClient('d6f7f400-06f2-49db-aab9-f7540f04f193', 'https://localhost', 'eb53fc5c-53b5-4e43-a52f-5bbba1bebce4')
 import numpy as np
 
-x = np.random.random(100)
-y = np.random.random(100)
+x = np.arange(100) / 6.0
+y = np.sin(x)
 data_source = p.make_source(idx=range(100), x=x, y=y)
 scatterplot1 = p.scatter(
-    x='idx', y='x', color='#F00',
+    x='x', y='y', color='#F00',
     data_source=data_source, title='paddy_from_py')
+
+scatterplot1 = p.line(
+    x='x', y='y', 
+    data_source=data_source, lineplot=scatterplot1)
+
 tableplot1 = p.table(
     x='idx', y='x', color='#F00',
     data_source=data_source, title='paddy_table_py')
