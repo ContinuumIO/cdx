90fc02b910f2659bf065decbbbcb19916ad285cb
Hugo <humongo.shi@gmail.com>
2012-12-10 11:39:36 -0500

    cleaning up environment code

--- startprod.py
+++ startprod.py
@@ -9,13 +9,9 @@ from wakariserver.flasklib import RequestSession
 import os
 import logging
 from logging import Formatter
-from wakariserver.environments import BASE_ENV, DEV, PROD, PROD_DEBUG
-BASE_ENV.LOG_FILE_NAME = "cdx.log"
-#can customize env hear if you want
-Envies = dict(DEV=DEV,
-              PROD=PROD,
-              PROD_DEBUG=PROD_DEBUG)
-ENV =  Envies[os.environ.get("WAKARI_ENV", "DEV")]()
+from wakariserver.environments import ENV
+import wakariserver.environments as environments
+environments.BASE_ENV.LOG_FILE_NAME = "cdx.log"
 
 start.prepare_app()
 app.debug = ENV.DEBUG
