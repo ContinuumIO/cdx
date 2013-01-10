37ba5b1481166cc76ffaa685a84f047426418f72
serverhacks <serverhacks>
2013-01-10 04:12:06 +0000

    fixed setup

--- setupcdxlib.py
+++ setupcdxlib.py
@@ -8,21 +8,21 @@ __version__ = (0, 0, 1)
 packages = ["cdxlib"]
 package_data_dirs = []
 for dirname, _, files in os.walk('cdxlib/static'):
-    dirname = os.path.relpath(dirname, 'cdx')
+    dirname = os.path.relpath(dirname, "cdxlib")    
     for f in files:
-        package_data_dirs.append(os.path.join(dirname, f))
+        package_data_dirs.append(os.path.join(dirname,f))
         
 for dirname, _, files in os.walk('cdxlib/templates'):
-    dirname = os.path.relpath(dirname, 'cdx')
+    dirname = os.path.relpath(dirname, "cdxlib")
     for f in files:
         package_data_dirs.append(os.path.join(dirname, f))
-
+print package_data_dirs
 setup(
     name = 'cdxlib',
     version = '.'.join([str(x) for x in __version__]),
     author = 'Continuum Analytics',
     packages = packages,
-    package_data = {'cdx' : package_data_dirs},    
+    package_data = {'cdxlib' : package_data_dirs},    
     author_email = '',
     url = '',
     description = 'CDX library',
