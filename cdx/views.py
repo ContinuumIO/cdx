95b2f883ac3edfd32424540ceccd9da37224dd6a
Hugo <humongo.shi@gmail.com>
2013-06-10 07:58:20 -0400

    using hemlib to setup contexts for hem css/js

--- cdx/views.py
+++ cdx/views.py
@@ -1,32 +1,21 @@
 from flask import (
     render_template, request,
     send_from_directory, make_response, abort,
-    jsonify
+    jsonify, current_app
     )
-
 from continuumweb import hemlib
-
 from app import cdx_app
+from bokeh.server.app import bokeh_app
 
-@cdx_app.route('/cdx/')
-def index(*unused_all, **kwargs):
-    if getattr(cdx_app, "debugjs", False):
-        slug = hemlib.slug_json()
-        static_js = hemlib.slug_libs(cdx_app, slug['libs'])
-        cssfiles =  [
-            "http://localhost:%s/css/application.css" % cdx_app.hem_port
-            ]
-        hem_js = hemlib.all_coffee_assets("localhost", cdx_app.hem_port)
-    else:
-        static_js = ['/cdx/static/js/application.js']
-        cssfiles =  ["/cdx/static/css/application.css"]
-        hem_js = []
-    return render_template(
-        'cdx.html',
-        jsfiles=static_js,
-        hemfiles=hem_js,
-        cssfiles=cssfiles
-        )
+def render(template, **kwargs):
+    context = hemlib.flask_template_context(cdx_app, cdx_app.hem_port)
+    context.update(kwargs)
+    return render_template(template, **context)
 
+
+@cdx_app.route('/cdx/<path:docname>')
+def index(docpath=None):
+    u = User.load(bokeh_app.model_redis, 
+    return render('cdx.html')
 
     
