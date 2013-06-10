05b94fa72b467102bac76e177bc6ebc149b6996e
Hugo <humongo.shi@gmail.com>
2013-06-10 09:26:31 -0400

    passed bokeh auth info to cdx split out bokeh init from cdx init

--- cdx/start.py
+++ cdx/start.py
@@ -5,6 +5,7 @@ import gevent
 import gevent.monkey
 gevent.monkey.patch_all()
 from app import cdx_app
+
 from bokeh.server.app import bokeh_app
 from bokeh.server.start import prepare_app as bokeh_prepare_app
 from bokeh.server.start import prepare_local as bokeh_prepare_local
@@ -17,13 +18,21 @@ HEM_PORT = 9394
 NB_PORT = 10010
 log = logging.getLogger(__name__)
 
-def prepare_app(rhost='127.0.0.1', rport=REDIS_PORT, debug=True, debugjs=True):
-    bokeh_prepare_app(rhost=rhost, rport=rport)
-    import views
-    bokeh_app.hem_port = HEM_PORT
-    cdx_app.hem_port = HEM_PORT
+def prepare_app(bokehhost='127.0.0.1',
+                bokehport='5006',
+                rhost='127.0.0.1', rport=REDIS_PORT,
+                debug=True, debugjs=True,
+                username='defaultuser',
+                userapikey='nokey',
+                ):
     app = Flask("cdx")
-    app.register_blueprint(bokeh_app)
+    prepare_bokeh(app, rhost=rhost, rport=rport,
+                  debug=debug, debugjs=debugjs)
+    #FIXME - serverloc should be computed
+    cdx_app.username = username
+    cdx_app.userapikey = userapikey
+    import views
+    cdx_app.hem_port = HEM_PORT
     app.register_blueprint(cdx_app)
     if debug:
         app.debug = True
@@ -34,6 +43,16 @@ def prepare_app(rhost='127.0.0.1', rport=REDIS_PORT, debug=True, debugjs=True):
         cdx_app.debugjs = True
         bokeh_app.debugjs = True
     return app
+
+def prepare_bokeh(app, rhost='127.0.0.1', rport=REDIS_PORT,
+                  debug=True, debugjs=True):
+    bokeh_prepare_app(rhost=rhost, rport=rport)
+    bokeh_app.hem_port = HEM_PORT
+    app.register_blueprint(bokeh_app)
+    if debug:
+        bokeh_app.debug = True
+    if debugjs:
+        bokeh_app.debugjs = True
 
 def start_app(app, verbose=False):
     global http_server
