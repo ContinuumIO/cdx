cfb581411286e5f24bf7be7bc6c52ffc9d562965
Hugo <humongo.shi@gmail.com>
2013-06-10 09:27:41 -0400

    bokeh info from cli

--- startlocal.py
+++ startlocal.py
@@ -2,10 +2,22 @@ from cdx import start
 from cdx.redisutils import RedisProcess
 from continuumweb import hemlib
 import os
-app = start.prepare_app()
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--username", 
+                    help="username for bokeh",
+                    default="defaultuser"
+                    )
+parser.add_argument("--userapikey",
+                    help="user apikey for bokeh",
+                    default="nokey"
+                    )
+args = parser.parse_args()
+app = start.prepare_app(args.username, args.userapikey)
 app.start_redis = True
 start.prepare_local()
 hemlib.slug_path = os.path.dirname(__file__)
+
 if __name__ == "__main__":
     start.start_app(app, verbose=True)
 
